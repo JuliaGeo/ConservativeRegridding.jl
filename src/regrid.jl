d26e59b3dae45f029716c443e97ec7a58858b660
Gregory Wagner <wagner.greg@gmail.com>
2025-04-11 11:57:39 -0400

    grid -> field

--- src/regrid.jl
+++ src/regrid.jl
@@ -1,56 +1,56 @@
 """$(TYPEDSIGNATURES)
-Regrid data on `grid_in` onto `grid_out` conservativly (mean-preserving) using the `regridder` matrix.
-`area_out` is the area of each grid cell in `grid_out` and is used to normalize the result,
+Regrid data on `field_in` onto `field_out` conservativly (mean-preserving) using the `regridder` matrix.
+`area_out` is the area of each grid cell in `field_out` and is used to normalize the result,
 if not provided, will recompute this from `regridder`."""
 Base.@propagate_inbounds function regrid!(
-    grid_out::AbstractVector,
-    grid_in::AbstractVector,
+    field_out::AbstractVector,
+    field_in::AbstractVector,
     regridder::AbstractMatrix,
     area_out::AbstractVector,
 )
-    @boundscheck (size(regridder, 2) == length(grid_out) && size(regridder, 1) == length(grid_in)) &&
-        @warn "regridder of size $(size(regridder)) matches input grid $(length(grid_in)) and output grid $(length(grid_out)) when transposed."*
+    @boundscheck (size(regridder, 2) == length(field_out) && size(regridder, 1) == length(field_in)) &&
+        @warn "regridder of size $(size(regridder)) matches input grid $(length(field_in)) and output grid $(length(field_out)) when transposed."*
                 " You may want to regrid with `transpose(regridder)` instead."
 
-    @boundscheck if length(grid_out) != length(area_out)
-        @warn "Area vector of length $(length(area_out)) is incompatible with output grid vector of length $(length(grid_out))."
+    @boundscheck if length(field_out) != length(area_out)
+        @warn "Area vector of length $(length(area_out)) is incompatible with output grid vector of length $(length(field_out))."
     end
     
     # the actual regridding is a matrix-vector multiplication with the regridder, do in-place
-    LinearAlgebra.mul!(grid_out, regridder, grid_in)    # units of grid_in times area of grid cell
-    grid_out ./= area_out                               # normalize by area of each grid cell
+    LinearAlgebra.mul!(field_out, regridder, field_in)    # units of field_in times area of grid cell
+    field_out ./= area_out                               # normalize by area of each grid cell
 end
 
 """$(TYPEDSIGNATURES)
-regrid a vector `grid_in` using `regridder` and writes the result in `grid_out`.
+regrid a vector `field_in` using `regridder` and writes the result in `field_out`.
 Recomputes the area vector for the output grid from `regridder`. Pass on as optional argument to avoid this."""
 function regrid!(
-    grid_out::AbstractVector,
-    grid_in::AbstractVector,
+    field_out::AbstractVector,
+    field_in::AbstractVector,
     regridder::AbstractMatrix,
 )
     area_out = cell_area(regridder, :out)
-    regrid!(grid_out, grid_in, regridder, area_out)
+    regrid!(field_out, field_in, regridder, area_out)
 end
 
 """$(TYPEDSIGNATURES)
-regrid a vector `grid_in` using `regridder`. Area vector for the output grid can
+regrid a vector `field_in` using `regridder`. Area vector for the output grid can
 be passed on as optional argument to prevent recalculating it from the regridder."""
 function regrid(
-    grid_in::AbstractVector,
+    field_in::AbstractVector,
     regridder::AbstractMatrix,
     args...
 )
     n_out, n_in = size(regridder)
-    @boundscheck if n_in != length(grid_in)
-        @warn "Regridder of size $(size(regridder)) does not match input grid of length $(length(grid_in))."
+    @boundscheck if n_in != length(field_in)
+        @warn "Regridder of size $(size(regridder)) does not match input grid of length $(length(field_in))."
     end
 
-    @boundscheck if n_out == length(grid_in)
-        @warn "Regridder of size $(size(regridder)) matches input grid of length $(length(grid_in)) if transposed." *
+    @boundscheck if n_out == length(field_in)
+        @warn "Regridder of size $(size(regridder)) matches input grid of length $(length(field_in)) if transposed." *
             " You may want to regrid with `transpose(regridder)` instead."
     end
 
-    grid_out = zeros(eltype(grid_in), n_out)    # default Julia vector for now
-    regrid!(grid_out, grid_in, regridder, args...)
+    field_out = zeros(eltype(field_in), n_out)    # default Julia vector for now
+    regrid!(field_out, field_in, regridder, args...)
 end
