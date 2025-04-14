1947e10ef03b1430d7b14727cf6bde9966f8e600
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-14 17:32:35 +0200

    add a test

--- test/oceananigans.jl
+++ test/oceananigans.jl
@@ -81,16 +81,41 @@ end
 coarse_grid = LatitudeLongitudeGrid(size=(90, 45, 1),   longitude=(0, 360), latitude=(-90, 90), z=(0, 1))
 fine_grid   = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), latitude=(-90, 90), z=(0, 1))
 
-c1 = CenterField(coarse_grid)
-c2 = CenterField(fine_grid)
+dst = CenterField(coarse_grid)
+src = CenterField(fine_grid)
 
-c1_cells = compute_cell_matrix(c1)
-c2_cells = compute_cell_matrix(c2)
+dst_cells = compute_cell_matrix(dst)
+src_cells = compute_cell_matrix(src)
 
-set!(c1, (x, y, z) -> rand())
+set!(src, (x, y, z) -> rand())
 
-regridder = ConservativeRegridding.Regridder(c1_cells, c2_cells)
+regridder = ConservativeRegridding.Regridder(dst_cells, src_cells)
 
-ConservativeRegridding.regrid!(vec(interior(c2)), transpose(regridder), vec(interior(c1)))
+ConservativeRegridding.regrid!(vec(interior(dst)), regridder, vec(interior(src)))
 
-@test 
+@test mean(dst) ≈ mean(src)
+
+set!(dst, (x, y, z) -> rand())
+
+ConservativeRegridding.regrid!(vec(interior(src)), transpose(regridder), vec(interior(dst)))
+
+@test mean(c2) ≈ mean(c1)
+
+# Does not pass until we figure out what to do for zero areas
+# large_domain_grid = RectilinearGrid(size=(100, 100), x=(0, 2), y=(0, 2), topology=(Periodic, Periodic, Flat))
+# small_domain_grid = RectilinearGrid(size=(200, 200), x=(0, 1), y=(0, 1), topology=(Periodic, Periodic, Flat))
+
+# src = CenterField(small_domain_grid)
+# dst = CenterField(large_domain_grid)
+
+# src_cells = compute_cell_matrix(src)
+# dst_cells = compute_cell_matrix(dst)
+
+# set!(src, 1)
+
+# regridder = ConservativeRegridding.Regridder(dst_cells, src_cells)
+
+# ConservativeRegridding.regrid!(vec(interior(dst)), regridder, vec(interior(src)))
+
+# @test mean(dst) ≈ mean(src)
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test, SafeTestsets
 @testset "ConservativeRegridding.jl" begin
     @safetestset "Simple Regridding" begin include("simple.jl") end
     @safetestset "SpeedyWeather" begin include("speedyweather.jl") end
+    @safetestset "Oceananigans" begin include("oceananigans.jl") end
 end
