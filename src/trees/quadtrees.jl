1e8831fbff36e4bf46740da1b2b6892e45636f19
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-01-09 11:19:11 -0100

    add docstrings + comments and minor refactor

--- src/trees/quadtrees.jl
+++ src/trees/quadtrees.jl
@@ -20,14 +20,25 @@ ncells(quadtree, dim::Int) -> Int
 ```
 
 and then you may also want to specialize on `STI.node_extent(::QuadtreeCursor{<: YourQuadtreeType}) -> GO.UnitSpherical.SphericalCap{Float64}`
-
-
 """
 abstract type AbstractQuadtree end
 
+"""
+    getcell(quadtree::AbstractQuadtree, i::Int, j::Int) -> GI.Polygon
+
+Get the cell at the given indices from the underlying quadtree object.
+"""
 function getcell(quadtree::AbstractQuadtree, i::Int, j::Int)
     error("getcell not implemented for $(typeof(quadtree))")
 end
+
+"""
+    ncells(quadtree::AbstractQuadtree, dim::Int) -> Int
+
+Get the number of cells in the given dimension of the underlying quadtree object.
+
+This is used to determine the size of the quadtree in the given dimension.
+"""
 function ncells(quadtree::AbstractQuadtree, dim::Int)
     error("ncells not implemented for $(typeof(quadtree))")
 end
@@ -37,6 +48,11 @@ function getcell(quadtree::AbstractQuadtree)
 end
 getcell(quadtree::AbstractQuadtree, idx::CartesianIndex{2}) = getcell(quadtree, idx[1], idx[2])
 
+"""
+    ExplicitPolygonQuadtree(polygons::AbstractMatrix)
+
+A quadtree that is built from a matrix of pre-computed polygons.  This is the most explicit method with the least optimizations, but it is the most flexible.
+"""
 struct ExplicitPolygonQuadtree{PolyMatrixType <: AbstractMatrix} <: AbstractQuadtree
     polygons::PolyMatrixType
 end
@@ -44,6 +60,12 @@ end
 getcell(quadtree::ExplicitPolygonQuadtree, i::Int, j::Int) = quadtree.polygons[i, j]
 ncells(quadtree::ExplicitPolygonQuadtree, dim::Int) = size(quadtree.polygons, dim)
 
+"""
+    CellBasedQuadtree(points::AbstractMatrix)
+
+A quadtree that is built from a matrix of corner points.  This is more optimized than [`ExplicitPolygonQuadtree`](@ref) 
+because it knows the corner points of each polygon.
+"""
 struct CellBasedQuadtree{PointMatrixType <: AbstractMatrix} <: AbstractQuadtree
     points::PointMatrixType
 end   
@@ -64,6 +86,15 @@ Base.@propagate_inbounds function getcell(quadtree::CellBasedQuadtree, i::Int, j
 end
 ncells(quadtree::CellBasedQuadtree, dim::Int) = size(quadtree.points, dim) - 1
 
+"""
+    RegularGridQuadtree(x::AbstractVector, y::AbstractVector)
+
+A quadtree that is built from a regular grid of x and y coordinates.  
+
+This is optimized for regular grids but requires unit-spherical transforms
+to be run on each call to `node_extent` - so it might be better to use a 
+[`CellBasedQuadtree`](@ref) instead if performance is a concern.
+"""
 struct RegularGridQuadtree{DX, DY} <: AbstractQuadtree
     x::DX
     y::DY
@@ -81,6 +112,15 @@ function getcell(quadtree::RegularGridQuadtree, i::Int, j::Int)
     ])])
 end
 
+"""
+    QuadtreeCursor(quadtree::AbstractQuadtree)
+
+A cursor for traversing the quadtree.  This is used to traverse the quadtree in a depth-first manner, and to get the child nodes of the current node.
+
+## Fields
+
+$(DocStringExtensions.FIELDS)
+"""
 struct QuadtreeCursor{QuadtreeType <: AbstractQuadtree}
     "The leaf level representation that the quadtree is built on top of."
     quadtree::QuadtreeType
@@ -160,11 +200,19 @@ using LinearAlgebra
 function circle_from_four_corners(corner_points, other_points)
     p1, p2, p3, p4 = GO.UnitSphereFromGeographic().(corner_points)
     center = LinearAlgebra.normalize((p1 .+ p2 .+ p3 .+ p4) ./ 4)
-    corner_distance = maximum(p -> GO.spherical_distance(center, p), (p1, p2, p3, p4))
+    # Midpoints of lines / edges
+    p12 = GO.UnitSpherical.slerp(p1, p2, 0.5)
+    p23 = GO.UnitSpherical.slerp(p2, p3, 0.5)
+    p34 = GO.UnitSpherical.slerp(p3, p4, 0.5)
+    p41 = GO.UnitSpherical.slerp(p4, p1, 0.5)
+    # Distance to the furthest corner or edge midpoint
+    corner_distance = maximum(p -> GO.spherical_distance(center, p), (p1, p2, p3, p4, p12, p23, p34, p41))
+    # Distance to the furthest other point
     other_distance = maximum(p -> GO.spherical_distance(center, p), GO.UnitSphereFromGeographic().(other_points); init = corner_distance)
+    # Distance to the furthest point
     distance = max(corner_distance, other_distance)
-    #The following is done to not miss intersections through numerical inaccuracies
-    return GO.UnitSpherical.SphericalCap(center, distance*1.0001)
+    #The `*1.0001` is done to not miss intersections through numerical inaccuracies
+    return GO.UnitSpherical.SphericalCap(center, distance*1.0001, max_radius = distance*1.0001)
 end
 function _corner_and_other_points_for_circle_from_corners(q::QuadtreeCursor{<: CellBasedQuadtree})
     if STI.isleaf(q)
@@ -176,10 +224,9 @@ function _corner_and_other_points_for_circle_from_corners(q::QuadtreeCursor{<: C
         psize = (ncells(q.quadtree, 1), ncells(q.quadtree, 2))
         
         # Compute and clamp all indices to polygon matrix bounds
-        imin = min((q.idx[1] - 1) * scale + 1, psize[1])
-        imax = min(q.idx[1] * scale, psize[1])
-        jmin = min((q.idx[2] - 1) * scale + 1, psize[2])
-        jmax = min(q.idx[2] * scale, psize[2])
+        irange, jrange = leaf_idxs(q)
+        imin, imax = extrema(irange)
+        jmin, jmax = extrema(jrange)
 
         quadtree_points = q.quadtree.points
         corner_points = (quadtree_points[imin, jmin], quadtree_points[imin, jmax], quadtree_points[imax, jmin], quadtree_points[imax, jmax])
@@ -212,15 +259,10 @@ function STI.node_extent(q::QuadtreeCursor{<: RegularGridQuadtree})
             return GO.UnitSpherical.SphericalCap(GO.UnitSphericalPoint((0.,0.,1.)), pi)
         end
     end
-    idx = q.idx.I
-    scale = 2^(q.level - 1)
-    psize = (ncells(q.quadtree, 1), ncells(q.quadtree, 2))
-    
     # Compute and clamp all indices to polygon matrix bounds
-    imin = min((q.idx[1] - 1) * scale + 1, psize[1])
-    imax = min(q.idx[1] * scale, psize[1])
-    jmin = min((q.idx[2] - 1) * scale + 1, psize[2])
-    jmax = min(q.idx[2] * scale, psize[2])
+    irange, jrange = leaf_idxs(q)
+    imin, imax = extrema(irange)
+    jmin, jmax = extrema(jrange)
 
     x, y = q.quadtree.x, q.quadtree.y
 
