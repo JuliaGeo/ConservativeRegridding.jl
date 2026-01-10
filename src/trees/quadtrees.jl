2636fc0f6039010342b71f3b03966532e8c2288a
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-01-09 23:12:20 -0100

    Add new top-down type quadtree cursor

--- src/trees/quadtrees.jl
+++ src/trees/quadtrees.jl
@@ -42,6 +42,17 @@ This is used to determine the size of the quadtree in the given dimension.
 function ncells(quadtree::AbstractQuadtree, dim::Int)
     error("ncells not implemented for $(typeof(quadtree))")
 end
+
+"""
+    cell_range_extent(quadtree::AbstractQuadtree, irange::UnitRange{Int}, jrange::UnitRange{Int}) -> GO.UnitSpherical.SphericalCap{Float64}
+
+Get the extent of the cells in the given range of indices.
+"""
+function cell_range_extent(quadtree::AbstractQuadtree, irange::UnitRange{Int}, jrange::UnitRange{Int})
+    error("cell_range_extent not implemented for $(typeof(quadtree))")
+end
+
+
 # Toplevel generic method to get all cells
 function getcell(quadtree::AbstractQuadtree)
     return (getcell(quadtree, i, j) for i in 1:ncells(quadtree, 1), j in 1:ncells(quadtree, 2))
@@ -59,6 +70,7 @@ end
 
 getcell(quadtree::ExplicitPolygonQuadtree, i::Int, j::Int) = quadtree.polygons[i, j]
 ncells(quadtree::ExplicitPolygonQuadtree, dim::Int) = size(quadtree.polygons, dim)
+
 
 """
     CellBasedQuadtree(points::AbstractMatrix)
@@ -112,6 +124,11 @@ function getcell(quadtree::RegularGridQuadtree, i::Int, j::Int)
     ])])
 end
 
+
+
+
+abstract type AbstractQuadtreeCursor end
+
 """
     QuadtreeCursor(quadtree::AbstractQuadtree)
 
@@ -121,7 +138,7 @@ A cursor for traversing the quadtree.  This is used to traverse the quadtree in 
 
 $(DocStringExtensions.FIELDS)
 """
-struct QuadtreeCursor{QuadtreeType <: AbstractQuadtree}
+struct QuadtreeCursor{QuadtreeType <: AbstractQuadtree} <: AbstractQuadtreeCursor
     "The leaf level representation that the quadtree is built on top of."
     quadtree::QuadtreeType
     "The index of the current cell at the current level in the quadtree."
@@ -129,6 +146,7 @@ struct QuadtreeCursor{QuadtreeType <: AbstractQuadtree}
     "The level of the cursor - 1 is the base i.e. smallest polygon level, as you increase the number you increase the size of the thing."
     level::Int
 end
+
 
 function QuadtreeCursor(quadtree::AbstractQuadtree)
     # Need 2^(level-1) >= max_size, so level >= log2(max_size) + 1
@@ -157,6 +175,10 @@ function getcell(q::QuadtreeCursor)
     return (getcell(q.quadtree, i, j) for i in is, j in js)
 end
 
+function getcell(q::QuadtreeCursor, i, j)
+    return getcell(q.quadtree, i, j)
+end
+
 STI.isspatialtree(::Type{<: QuadtreeCursor}) = true
 function STI.nchild(q::QuadtreeCursor)
     imax, jmax = (q.idx.I .- 1) .* (2^q.level) .+ 1
@@ -180,8 +202,16 @@ function STI.nchild(q::QuadtreeCursor)
 end
 
 function STI.isleaf(q::QuadtreeCursor)
-    q.level < 1 && throw(ArgumentError("Quadtree level must be greater than 1; got $(q.level).  Something went wrong!"))
-    return q.level == 1
+    # q.level < 2 && throw(ArgumentError("Quadtree level must be greater than 1; got $(q.level).  Something went wrong!"))
+    return q.level <= 2
+end
+
+function STI.child_indices_extents(q::QuadtreeCursor)
+    @assert q.level == 2 "Child indices and extents are only valid for level 2 quadtrees cursors, i.e., nodes with children."
+    irange, jrange = leaf_idxs(q)
+    idxs = CartesianIndices((irange, jrange))
+    extents = [STI.node_extent(STI.getchild(q, i)) for i in 1:STI.nchild(q)]
+    return zip(idxs, extents)
 end
 
 function STI.getchild(q::QuadtreeCursor, i::Int)
@@ -212,17 +242,15 @@ function circle_from_four_corners(corner_points, other_points)
     # Distance to the furthest point
     distance = max(corner_distance, other_distance)
     #The `*1.0001` is done to not miss intersections through numerical inaccuracies
-    return GO.UnitSpherical.SphericalCap(center, distance*1.0001, max_radius = distance*1.0001)
+    return GO.UnitSpherical.SphericalCap(center, distance*1.0001)
 end
 function _corner_and_other_points_for_circle_from_corners(q::QuadtreeCursor{<: CellBasedQuadtree})
     if STI.isleaf(q)
-        pts = GI.getpoint.(([getcell(q.quadtree, q.idx[1], q.idx[2])],), 1:4)
+        idx = q.idx.I
+        pmat = q.quadtree.points
+        pts = (pmat[idx[1], idx[2]], pmat[idx[1], idx[2]+1], pmat[idx[1]+1, idx[2]+1], pmat[idx[1]+1, idx[2]])
         return (pts, pts)
-    else
-        # Calculate the range of leaf indices covered by this node
-        scale = 2^(q.level - 1)
-        psize = (ncells(q.quadtree, 1), ncells(q.quadtree, 2))
-        
+    else        
         # Compute and clamp all indices to polygon matrix bounds
         irange, jrange = leaf_idxs(q)
         imin, imax = extrema(irange)
@@ -245,6 +273,55 @@ function _corner_and_other_points_for_circle_from_corners(q::QuadtreeCursor{<: C
         
         return (corner_points, other_points)
     end
+end
+
+function cell_range_extent(q::CellBasedQuadtree, irange::UnitRange{Int}, jrange::UnitRange{Int})
+    imin, imax = extrema(irange)
+    jmin, jmax = extrema(jrange)
+    # For cell based we need 1 to the max indices from cell space 
+    # to get the max indices in point space (`(n,m) -> (n+1, m+1)`)
+    imax += 1
+    jmax += 1
+
+    quadtree_points = q.points
+    corner_points = (quadtree_points[imin, jmin], quadtree_points[imin, jmax], quadtree_points[imax, jmin], quadtree_points[imax, jmax])
+
+    # Collect points from all border polygons
+    other_points = typeof(GI.getpoint(GI.getexterior(getcell(q, imin, jmin)), 1))[]
+    sizehint!(other_points, (imax - imin + 1) * (jmax - jmin + 1))
+    # Top and bottom rows (all columns)
+    append!(other_points, view(q.points, imin, jmin:jmax))
+    if imax != imin
+        append!(other_points, view(q.points, imax, jmin:jmax))
+    end
+    if jmax != jmin
+        append!(other_points, view(q.points, imin:imax, jmax))
+    end
+    return circle_from_four_corners(corner_points, other_points)
+end
+
+function cell_range_extent(q::RegularGridQuadtree, irange::UnitRange{Int}, jrange::UnitRange{Int})
+    imin, imax = extrema(irange)
+    jmin, jmax = extrema(jrange)
+    # For cell based we need 1 to the max indices from cell space 
+    # to get the max indices in point space (`(n,m) -> (n+1, m+1)`)
+    imax += 1
+    jmax += 1
+
+    corner_points = ((q.x[imin], q.y[jmin]), (q.x[imin], q.y[jmax]), (q.x[imax], q.y[jmin]), (q.x[imax], q.y[jmax]))
+
+    # Collect points from all border polygons
+    other_points = typeof(GI.getpoint(GI.getexterior(getcell(q, imin, jmin)), 1))[]
+    sizehint!(other_points, (imax - imin + 1) * (jmax - jmin + 1))
+    # Top and bottom rows (all columns)
+    append!(other_points, tuple.(q.x[imin], q.y[jmin:jmax]))
+    if imax != imin
+        append!(other_points, tuple.(q.x[imax], q.y[jmin:jmax]))
+    end
+    if jmax != jmin
+        append!(other_points, tuple.(q.x[imin:imax], q.y[jmax]))
+    end
+    return circle_from_four_corners(corner_points, other_points)
 end
 
 function STI.node_extent(q::QuadtreeCursor{<: CellBasedQuadtree})
@@ -270,4 +347,94 @@ function STI.node_extent(q::QuadtreeCursor{<: RegularGridQuadtree})
         ((x[imin], y[jmin]), (x[imin], y[jmax]), (x[imax], y[jmin]), (x[imax], y[jmax])),
         ()
     )
+end
+
+
+
+
+struct TopDownQuadtreeCursor{QuadtreeType <: AbstractQuadtree} <: AbstractQuadtreeCursor
+    quadtree::QuadtreeType
+    leafranges::NTuple{2, UnitRange{Int}}
+end
+function TopDownQuadtreeCursor(quadtree::AbstractQuadtree)
+    return TopDownQuadtreeCursor(quadtree, (1:ncells(quadtree, 1), 1:ncells(quadtree, 2)))
+end
+
+function Base.show(io::IO, q::TopDownQuadtreeCursor)
+    print(io, "TopDownQuadtreeCursor(($(q.leafranges[1])), ($(q.leafranges[2])))")
+end
+function Base.show(io::IO, ::MIME"text/plain", q::TopDownQuadtreeCursor)
+    print(io, "TopDownQuadtreeCursor(($(q.leafranges[1])), ($(q.leafranges[2])))")
+end
+
+STI.isspatialtree(::Type{<: TopDownQuadtreeCursor}) = true
+
+function STI.isleaf(q::TopDownQuadtreeCursor)
+    return all(length.(q.leafranges) .<= 2)
+end
+
+function STI.child_indices_extents(q::TopDownQuadtreeCursor)
+    idxs = ((i, j) for i in q.leafranges[1], j in q.leafranges[2])
+    extents = (cell_range_extent(q.quadtree, i:i, j:j) for i in q.leafranges[1], j in q.leafranges[2])
+    return zip(idxs, extents)
+end
+
+function STI.nchild(q::TopDownQuadtreeCursor)
+    i_is_one = length(q.leafranges[1]) == 1 # length-1 in i
+    j_is_one = length(q.leafranges[2]) == 1 # length-1 in j
+
+    if i_is_one && j_is_one
+        error("This should be unreachable - `irange` is length 1 and so is `jrange`")
+    elseif i_is_one
+        return 2
+    elseif j_is_one
+        return 2
+    else
+        return 4
+    end
+end
+
+function STI.getchild(q::TopDownQuadtreeCursor, i::Int)
+    i_is_one = length(q.leafranges[1]) == 1 # length-1 in i
+    j_is_one = length(q.leafranges[2]) == 1 # length-1 in j
+
+    vals = if i_is_one && j_is_one
+        error("This should be unreachable - `irange` is length 1 and so is `jrange`")
+    elseif i_is_one
+        j_split_point = length(q.leafranges[2]) ÷ 2
+        (
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1], q.leafranges[2][1:j_split_point])),
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1], q.leafranges[2][j_split_point+1:end]))
+        )
+    elseif j_is_one
+        i_split_point = length(q.leafranges[1]) ÷ 2
+        (
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][1:i_split_point], q.leafranges[2])),
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][i_split_point+1:end], q.leafranges[2]))
+        )
+    else
+        i_split_point = length(q.leafranges[1]) ÷ 2
+        j_split_point = length(q.leafranges[2]) ÷ 2
+        (
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][1:i_split_point], q.leafranges[2][1:j_split_point])),
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][1:i_split_point], q.leafranges[2][j_split_point+1:end])),
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][i_split_point+1:end], q.leafranges[2][1:j_split_point])),
+            TopDownQuadtreeCursor(q.quadtree, (q.leafranges[1][i_split_point+1:end], q.leafranges[2][j_split_point+1:end]))
+        )
+    end
+    return vals[i]
+end
+
+function STI.getchild(q::TopDownQuadtreeCursor)
+    return (STI.getchild(q, i) for i in 1:STI.nchild(q))
+end
+
+function STI.node_extent(q::TopDownQuadtreeCursor)
+    return cell_range_extent(q.quadtree, q.leafranges[1], q.leafranges[2])
+end
+
+
+
+function getcell(q::TopDownQuadtreeCursor)
+    return (getcell(q.quadtree, i, j) for i in q.leafranges[1], j in q.leafranges[2])
 end
