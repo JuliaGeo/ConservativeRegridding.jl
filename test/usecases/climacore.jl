f86aa78e83687f6dffacb55e994cb0a567f86681
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-01-17 08:07:13 -0500

    Split out tests to try to get them to pass

--- test/usecases/climacore.jl
+++ test/usecases/climacore.jl
@@ -110,39 +110,6 @@ end
 
 # ## Metrics
 
-
-@testset "Test helper functions in extension" begin
-    cubedsphere_space = CommonSpaces.CubedSphereSpace(;
-        radius = latlon_grid.radius,
-        n_quad_points = 2,
-        h_elem = 64,
-    )
-
-    # Define a field on the first space, to use as our source field
-    field = Fields.coordinate_field(cubedsphere_space).long
-    ones_field = Fields.ones(cubedsphere_space)
-    cubed_sphere_vals = zeros(6*cubedsphere_space.grid.topology.mesh.ne^2)
-    ClimaCoreExt.get_value_per_element!(cubed_sphere_vals, field, ones_field)
-
-    @testset "integrate_each_element" begin
-        @test isapprox(
-            sum(ClimaCoreExt.integrate_each_element(field)), 
-            sum(field), 
-            atol = 1e-11
-        )
-        @test sum(ClimaCoreExt.integrate_each_element(ones_field)) ≈ sum(ones_field)
-    end
-    @testset "Convert to one value per element" begin
-        value_per_element1 = zeros(Float64, Meshes.nelements(cubedsphere_space.grid.topology.mesh))
-        field1_one_value_per_element = Fields.zeros(cubedsphere_space)
-        ClimaCoreExt.set_value_per_element!(field1_one_value_per_element, value_per_element1)
-
-        # Check the error of converting to one value per element
-        abs_error_one_value_per_element = abs(sum(field1_one_value_per_element) - sum(field))
-        @test_broken abs_error_one_value_per_element < 2e-11
-        @test isapprox(mean(field), mean(field1_one_value_per_element), atol=1e-14)
-    end
-end
 #=
 using GLMakie, GeoMakie
 
