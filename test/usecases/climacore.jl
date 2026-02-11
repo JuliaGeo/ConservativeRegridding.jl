64fc950b06df8190d3f7910ddeb3867f5f6efaa6
Julia Sloan <jsloan@caltech.edu>
2026-02-10 16:37:18 -0800

    improve ClimaCore tests

--- test/usecases/climacore.jl
+++ test/usecases/climacore.jl
@@ -12,23 +12,30 @@ const ClimaCoreExt = Base.get_extension(ConservativeRegridding, :ConservativeReg
 
 latlon_grid = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), latitude=(-90, 90), z = (0, 1), radius = GO.Spherical().radius)
 
-@testset "Regular cubed sphere" begin
+@testset "Regular cubed sphere (without spacefillingcurve)" begin
+    context = ClimaComms.context()
+    h_elem = 16
+    h_mesh = Meshes.EquiangularCubedSphere(Domains.SphereDomain{Float64}(GO.Spherical().radius), h_elem)
+    h_topology = Topologies.Topology2D(context, h_mesh)
     cubedsphere_space = CommonSpaces.CubedSphereSpace(;
-        radius = latlon_grid.radius,
+        radius = h_mesh.domain.radius,
         n_quad_points = 2,
-        h_elem = 64,
+        h_elem,
+        h_mesh,
+        h_topology,
     )
+
+    @assert !Topologies.uses_spacefillingcurve(cubedsphere_space.grid.topology)
 
     # Define a field on the first space, to use as our source field
     field = Fields.coordinate_field(cubedsphere_space).long
+
     ones_field = Fields.ones(cubedsphere_space)
-    cubed_sphere_vals = zeros(6*cubedsphere_space.grid.topology.mesh.ne^2)
+    cubed_sphere_vals = zeros(Meshes.nelements(cubedsphere_space.grid.topology.mesh))
     ClimaCoreExt.get_value_per_element!(cubed_sphere_vals, field, ones_field)
-    cubed_sphere_vals
 
     latlon_field = Oceananigans.CenterField(latlon_grid)
     latlon_vals = vec(interior(latlon_field))
-    # heatmap(interior(latlon_field, :, :, 1))
     set!(latlon_field, (x, y, z) -> 0)
 
     regridder = ConservativeRegridding.Regridder(latlon_grid, cubedsphere_space; threaded = false)
@@ -39,12 +46,10 @@ latlon_grid = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), lati
     @test isapprox(
         sum(abs, latlon_vals .* ConservativeRegridding.areas(GO.Spherical(), Trees.treeify(latlon_grid))),
         sum(abs, cubed_sphere_vals .* ConservativeRegridding.areas(GO.Spherical(), Trees.treeify(cubedsphere_space))),
-        rtol = 1e-13
+        rtol = 1e-10
     )
 
     # Test the other way
-    set!(latlon_field, (x, y, z) -> x)
-
     ConservativeRegridding.regrid!(cubed_sphere_vals, transpose(regridder), latlon_vals)
 
     @test isapprox(
@@ -52,13 +57,22 @@ latlon_grid = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), lati
         sum(abs, latlon_vals .* ConservativeRegridding.areas(GO.Spherical(), Trees.treeify(latlon_grid))),
         rtol = 1e-13
     )
-end
-# With a space filling curve
 
-@testset "Gilbert ordered cubed sphere" begin
-    device = ClimaComms.device()
-    context = ClimaComms.context(device)
-    h_elem = 64
+    field_remapped = Fields.zeros(cubedsphere_space)
+    ClimaCoreExt.set_value_per_element!(field_remapped, cubed_sphere_vals)
+
+    # Check that the integral over the space is conserved
+    # TODO broken: -2 vs -4
+    @test_broken isapprox(
+        sum(field),
+        sum(field_remapped),
+        rtol = 1e-6,
+    )
+end
+
+@testset "Gilbert ordered cubed sphere (with spacefillingcurve)" begin
+    context = ClimaComms.context()
+    h_elem = 16
     h_mesh = Meshes.EquiangularCubedSphere(Domains.SphereDomain{Float64}(GO.Spherical().radius), h_elem)
     h_topology = Topologies.Topology2D(context, h_mesh, Topologies.spacefillingcurve(h_mesh))
     cubedsphere_gilbert_ordered_space = CommonSpaces.CubedSphereSpace(;
@@ -69,21 +83,20 @@ end
         h_topology = h_topology,
     )
 
+    @assert Topologies.uses_spacefillingcurve(cubedsphere_gilbert_ordered_space.grid.topology)
 
     # Define a field on the first space, to use as our source field
     field = Fields.coordinate_field(cubedsphere_gilbert_ordered_space).long
+
     ones_field = Fields.ones(cubedsphere_gilbert_ordered_space)
-    cubed_sphere_vals = zeros(6*cubedsphere_gilbert_ordered_space.grid.topology.mesh.ne^2)
+    cubed_sphere_vals = zeros(Meshes.nelements(cubedsphere_gilbert_ordered_space.grid.topology.mesh))
     ClimaCoreExt.get_value_per_element!(cubed_sphere_vals, field, ones_field)
-    cubed_sphere_vals
 
     latlon_field = Oceananigans.CenterField(latlon_grid)
     latlon_vals = vec(interior(latlon_field))
-    # heatmap(interior(latlon_field, :, :, 1))
     set!(latlon_field, (x, y, z) -> 0)
 
     regridder = ConservativeRegridding.Regridder(latlon_grid, cubedsphere_gilbert_ordered_space; threaded = false)
-    # regridder = ConservativeRegridding.Regridder(cubedsphere_gilbert_ordered_space, latlon_grid; threaded = false)
 
     ConservativeRegridding.regrid!(latlon_vals, regridder, cubed_sphere_vals)
 
@@ -95,14 +108,23 @@ end
     )
 
     # Test the other way
-    set!(latlon_field, (x, y, z) -> x)
-
     ConservativeRegridding.regrid!(cubed_sphere_vals, transpose(regridder), latlon_vals)
 
     @test isapprox(
         sum(abs, cubed_sphere_vals .* ConservativeRegridding.areas(GO.Spherical(), Trees.treeify(cubedsphere_gilbert_ordered_space))),
         sum(abs, latlon_vals .* ConservativeRegridding.areas(GO.Spherical(), Trees.treeify(latlon_grid))),
         rtol = 1e-13
+    )
+
+    field_remapped = Fields.zeros(cubedsphere_gilbert_ordered_space)
+    ClimaCoreExt.set_value_per_element!(field_remapped, cubed_sphere_vals)
+
+    # Check that the integral over the space is conserved
+    # TODO broken: -6 vs -3
+    @test_broken isapprox(
+        sum(field),
+        sum(field_remapped),
+        rtol = 1e-8,
     )
 end
 
