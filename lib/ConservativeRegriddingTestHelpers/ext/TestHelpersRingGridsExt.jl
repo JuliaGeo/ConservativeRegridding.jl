eada92b920b1cf36c35994713e6fa30b6901af3c
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-06-30 18:44:44 -0400

    Fix TestHelpersRingGrids dispatches to also work on fields

--- lib/ConservativeRegriddingTestHelpers/ext/TestHelpersRingGridsExt.jl
+++ lib/ConservativeRegriddingTestHelpers/ext/TestHelpersRingGridsExt.jl
@@ -4,8 +4,10 @@ import RingGrids
 
 import ConservativeRegriddingTestHelpers as TestHelpers
 
-TestHelpers.has_full_ring_grid(field::RingGrids.AbstractField) = field.grid isa RingGrids.AbstractFullGrid
+TestHelpers.has_full_ring_grid(field::RingGrids.AbstractField) = TestHelpers.has_full_ring_grid(field.grid)
+TestHelpers.has_full_ring_grid(field::RingGrids.AbstractFullGrid) = true
 
+TestHelpers.has_cell_crossing_dateline(field::RingGrids.AbstractField) = TestHelpers.has_cell_crossing_dateline(field.grid)
 TestHelpers.has_cell_crossing_dateline(field::RingGrids.AbstractFullGrid) = true
 TestHelpers.has_cell_crossing_dateline(field::RingGrids.HEALPixGrid) = true 
 TestHelpers.has_cell_crossing_dateline(field::RingGrids.OctaHEALPixGrid) = false # OctaHEALPix faces line up with meridians sometimes
